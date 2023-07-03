a45636fb424f26344aecf903c50035a92a08347a
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-03 22:20:50 +0530

    [Shivam Tripathi] : Added Address Book Contact form UC2

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace AddressBookProblem
 {
+
     public class AddressBook
     {
+        List<Contact> addressBookList = new List<Contact>();
+
         public void CreateContact()
+
         {
             Contact contacts = new Contact();
 
@@ -29,7 +33,43 @@ namespace AddressBookProblem
             Console.WriteLine("Enter Zip Code : ");
             contacts.ZipCode = Console.ReadLine();
             Console.WriteLine("Contact created successfully......");
-
+        }
+        public void AddContact()
+        {
+            Contact addContact = new Contact();
+            Console.WriteLine("Enter First Name : ");
+            addContact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            addContact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            addContact.Email = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number : ");
+            addContact.PhoneNo = Console.ReadLine();
+            Console.WriteLine("Enter the Address : ");
+            addContact.Address = Console.ReadLine();
+            Console.WriteLine("Enter State Name : ");
+            addContact.State = Console.ReadLine();
+            Console.WriteLine("Enter the City Name : ");
+            addContact.City = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code : ");
+            addContact.ZipCode = Console.ReadLine();
+            addressBookList.Add(addContact);
+            Console.WriteLine("Contact Added Successfully!");
+        }
+        public void DisplayContact()
+        {
+            Console.WriteLine("Contact Details");
+            foreach (Contact display in addressBookList)
+            {
+                Console.WriteLine(display.FirstName);
+                Console.WriteLine(display.LastName);
+                Console.WriteLine(display.Email);
+                Console.WriteLine(display.PhoneNo);
+                Console.WriteLine(display.Address);
+                Console.WriteLine(display.State);
+                Console.WriteLine(display.City);
+                Console.WriteLine(display.ZipCode);
+            }
 
         }
     }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -6,9 +6,32 @@
         {
             Console.WriteLine("...Welcome to Address Book...");
             AddressBook addressBook = new AddressBook();
-            addressBook.CreateContact();
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("1.Create Contact \n2.Add Contact \n3.Display Contact \n4.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBook.CreateContact();
+                        break;
+                    case 2:
+                        addressBook.AddContact();
+                        break;
+                    case 3:
+                        addressBook.DisplayContact();
+                        break;
+                    case 4:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct number ! ");
+                        break;
+                }
+            }
             Console.ReadLine();
-
         }
     }
+
 }
